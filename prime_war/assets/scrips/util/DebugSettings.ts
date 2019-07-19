091c2749ee95b00e220b89e7b28a83750bdcf33a
LucassLi <598979698@qq.com>
2019-07-19 19:48:08 +0800

    添加priem_war项目

---
+++ prime_war/assets/scrips/util/DebugSettings.ts
@@ -0,0 +1,11 @@
+// Copyright (C) 2019, Flickering Inc. All rights reserved.
+// Author: Kun Huang (huangkun1988@gmail.com)
+
+/** 调试输出设置 */
+export class DebugSettings {
+    public static debugNormal: boolean = false;
+    public static debugCollision: boolean = true;
+    public static debugEvent: boolean = true;
+    public static showConfig: boolean = false;
+
+}
